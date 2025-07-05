0d86cf07a66a31db0808636496ddd0ea0ff9f96b
ferdian32 <fierliferdiansyah0@gmail.com>
2025-07-05 19:37:21 +0700

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import HelloSvelte from "./lib/HelloSvelte.svelte";
+  const counter = 0;
+  console.log(counter);
+</script>
+
+<main>
+  <div>
+    <h1>Hello ini Svelte JS</h1>
+    <p>membuat web menggunakan Svelte</p>
+  </div>
+  <HelloSvelte />
+</main>
+
+<style>
+  h1 {
+    color: red;
+    text-align: center;
+    font-size: 80px;
+  }
+</style>
---
+++ src/lib/HelloSvelte.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Logo from "./Logo.svelte";
+    const name = 'Svelte';
+    const paragrafElement = '<p>Membuat html tags</p>';
+</script>   
+
+
+<style>
+
+</style>
+
+<section>
+    <!-- gunakan sebuah @html agar tag htmlnya merender sebuah value -->
+    {@html paragrafElement}
+    <h1>Hello {name.toUpperCase()}!</h1>
+    <Logo />
+</section>
